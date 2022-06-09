409eb3a66d9aba2777bbfad3d6d987c0c8fb03b4
Omar Bautista <oabm77@gmail.com>
2022-06-09 16:21:23 -0500

    added invitation

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Trying this crash course.
+          Trying this crash course. You are invited!
         </p>
       </header>
     </div>
